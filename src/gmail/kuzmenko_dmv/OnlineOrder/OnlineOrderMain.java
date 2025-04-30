c2b4dae90b6d6bfcc6d5ba90bf65e60739b25de0
NataliaKuzmenko-dvm <kuzmenkonataliia438@gmail.com>
2025-04-30 15:23:02 +0300

    Initial commit

---
+++ src/gmail/kuzmenko_dmv/OnlineOrder/OnlineOrderMain.java
@@ -0,0 +1,21 @@
+package gmail.kuzmenko_dmv.OnlineOrder;
+
+public class OnlineOrderMain {
+    public static void main(String[] args) {
+        // Створення масивів для зберігання даних
+        String[] clients = {"Alice", "Tom"};
+        String[] products = {"smartphone", "laptop"};
+        double[] firstPrice = {305.99, 570.95};
+        String[] addresses = {"Moon Street, 10", "Tera Street, 17"};
+
+        // Виведення замовлень
+        for (int orderNumber = 0; orderNumber < clients.length; orderNumber++) {
+            System.out.println("Order No " + (orderNumber + 1) + " + Client: " + clients[orderNumber] + ".");
+            System.out.println("Product: " + products[orderNumber] + ",");
+            System.out.println("firstPrice EUR: " + firstPrice[orderNumber] + ".");
+            System.out.println("Address: " + addresses[orderNumber] + ".");
+            System.out.println(); // порожній рядок для розділення
+        }
+    }
+}
+
---
+++ src/gmail/kuzmenko_dmv/OnlineOrder/lecture3/GitMyMain.java
@@ -0,0 +1,8 @@
+package gmail.kuzmenko_dmv.OnlineOrder.lecture3;
+
+public class GitMyMain {
+    public static void main(String args[]) {
+        System.out.println("Shop-online for customers");
+    }
+}
+
